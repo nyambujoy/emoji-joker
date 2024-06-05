91022e9e11c35812899e70e702952eeaf94100b3
nyambujoy <joyliznyambura@gmail.com>
2024-06-05 12:49:30 +0300

    finished styling

---
+++ app.js
@@ -0,0 +1,30 @@
+const url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
+const randomBtn = document.querySelector('.next')
+const joke = document.querySelector('.joke')
+
+console.log(joke, randomBtn)
+
+
+randomBtn.addEventListener('click', () => {
+    console.log('hello')
+    handleData()
+
+})
+
+
+
+async function handleData() {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        joke.textContent = 'sorry server error'
+    }
+    const data = await response.json()
+    console.log(data)
+    handleDisplay(data)
+    return data
+}
+
+function handleDisplay(data) {
+    joke.textContent = data.joke
+}
